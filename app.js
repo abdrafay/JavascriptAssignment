45a17de8e2d200ffa45c0bae2da2310fd2eea838
abdrafay <65040443+abdrafay@users.noreply.github.com>
2020-06-15 21:57:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//  1st Chap
+alert("Welcome to our Website");
+alert("Error! Please Enter a Valid Password");
+alert("Welcome to Js Land <br/> Happy Coding");
+alert("Welcome to Js Land");
+alert("Happy Coding");
+console.log(alert("Hello... I can run JS through my web browser's console"));
+
+// 2nd Chap
+var username;
+var myName = "Abdul Rafay";
+var message;
+message = "Hello World";
+
+alert(message);
+
+var studentName = "John Doe";
+var studentAge = "15 years Old";
+var certification = "Certified Hybrid Application Development";
+
+var pizza = "PIZZA <br /> PIZZ <br /> PIZ <br /> PI <br /> P";
+
+alert(pizza)
+
+var email = "zainrafay12@gmail.com";
+
+alert("Your Email Address is " + email);    
+var book = "A smarter way to learn JavaScript";
+
+document.write("Yah! I can write HTML content throught JavaScript");
+
+var alert2012 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬“";
+alert(alert2012);
+
+// 3rd Chap
+
+var age = "I am 15 years old";
+alert(age);
+
+var ntime = "You have visited this website 14 times";
+
+alert(ntime);
+
+var birthYear = "My birth Year is 1990";
+alert(birthYear + "<br>" + " Data type of my declared variable is number");
+
+document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+
+// 4th Chap
+
+var variable1 = "Hello World",
+    varibable2 = "Hello WOrld 2",
+    varibable3 = "Hello World 3";
+
+//    Assigned Illegal Variables 
+
+    // var 1name = "1st name";
+// var 2name = "2nd name";
+// var @name = "3rd name";
+// var name-1 = "4th name";
+// var #name = "5th name";
+
+var firstName = "1stname";
+var secondName = "2ndname";
+var thirdName = "3rdname";
+var forthName = "4thname";
+var fifthName = "5thname";
+
+document.write("<h1>Rules for naming JS variables</h1>")
+
+document.write("Variable names can only contain , numbers, $ and _. For Example: $my_1stVariable");
+
+
+document.write("Variable must begin with a letter, $, or _. For Example: $name, _name or name  ");
+
+document.write("Varibale names are case sensitive");
+
+document.write("Variable names should not be JS keywords");
